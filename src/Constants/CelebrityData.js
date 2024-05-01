f6d1916a6b79eb308e10ddd38b8dbd8983926c4f
Diveshkedisingh <deepakkr040404@gmail.com>
2024-05-01 14:24:51 +0530

    Cleaned up our aboutus page

---
+++ src/Constants/CelebrityData.js
@@ -0,0 +1,43 @@
+import nelsonMandela from "../Assets/Images/nelsonmandela.png"
+import apj from "../Assets/Images/apj.jpg";
+import einstein from "../Assets/Images/einstein.jpg";
+import stevejobs from "../Assets/Images/stevejobs.jpeg";
+import billgates from "../Assets/Images/billgates.jpg";
+export const celebrities = [
+        
+    {
+        title: "Nelson Mandela",
+        description:  "Education is the most powerful tool you can use to change the world.",
+        image: nelsonMandela,
+        slideNumber: 1
+    },
+
+    {
+        title: "APJ Abdul Kalam",
+        description:  "Failure will never overtake me if my determination to succeed is strong enough.",
+        image: apj,
+        slideNumber: 2
+    },
+
+    {
+        title: "Albert Einstein",
+        description:  "A person who never made a mistake never tried anything new.",
+        image: einstein,
+        slideNumber: 3
+    },
+
+    {
+        title: "Steve Jobs",
+        description:  "We don't get a chance to do that many things , and every one should be really excellent.",
+        image: stevejobs,
+        slideNumber: 4
+    },
+
+    {
+        title: "Bill Gates",
+        description:  "Success is a lousy teacher. it seduces smart people into thinking they can't lose.",
+        image: billgates,
+        slideNumber: 5
+    },
+    
+]
