d9d7db299166f8a826a5a7c42fbb2a152154c30e
Diveshkedisingh <deepakkr040404@gmail.com>
2024-05-05 03:36:55 +0530

    added contact page

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,7 @@ import Signup from './Pages/Signup';
 import './App.css';
 import Login from './Pages/Login';
 import CourseList from './Pages/Course/CourseList';
+import Contact from './Pages/Contact';
 
 
 
@@ -19,6 +20,8 @@ function App() {
            <Route path="/" element = {<HomePage /> }></Route>
            <Route path="/about" element = {<AboutUs /> }></Route>
            <Route path="/courses" element={<CourseList />}></Route>
+
+           <Route path="/contact" element={<Contact />}></Route>
 
 
            <Route path="/signup" element={<Signup />}></Route>
